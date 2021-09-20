a780758db39a40fb0f70be2450a0bc140eeb23b4
Mehrdad Amini <pcmehrdad@gmail.com>
2021-09-21 01:59:19 +0430

    run basic migrate and npm commands

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+/*Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
-})->name('dashboard');
+})->name('dashboard');*/
+
+Route::group(['prefix' => 'admin','as'=>'admin.'], function (){
+    //Route::get('dashboard')->name('dashboard');
+});
+
