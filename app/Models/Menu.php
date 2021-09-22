5a63ad175a04e432a640bd7cde272d2f4fae3ba5
Mehrdad Amini <pcmehrdad@gmail.com>
2021-09-22 19:01:48 +0330

    update

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Http\Traits\HasRecursiveRelationships;
+
 // https://github.com/paxha/laravel-recursive-relationships
+// https://newbedev.com/how-to-create-a-nested-list-of-categories-in-laravel
 
 class Menu extends Model
 {
@@ -24,6 +26,7 @@ class Menu extends Model
 class MenuItem extends Model
 {
     use HasFactory,HasRecursiveRelationships;
+
     protected $guarded = ['id'];
     public $timestamps = false;
 
